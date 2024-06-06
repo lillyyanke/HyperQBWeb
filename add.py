3c25082d58fd72e7db010057c047ad86714f146f
tzuhancs <tzuhan@msu.edu>
2024-06-06 12:55:37 -0400

    task2

---
+++ add.py
@@ -0,0 +1,8 @@
+import sys
+
+def add_numbers():
+    x = sys.argv[1]  #change this to what's in "model"
+    y = sys.argv[2]  #change this to what's in "property" 
+    return x + y
+
+print(add_numbers()) #change this to what's in displayed "Result"
